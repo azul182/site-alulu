123677eea4ec39fa223964dd5d09145242895f37
azul182 <84513231+azul182@users.noreply.github.com>
2022-04-28 09:45:17 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const conexao = async() => {
+    //conexao local
+    //var bdlocal = await mongoose.connect('mongodb://localhost/ac1tri')
+    //conexao atlas
+    var atlas = await mongoose.connect('mongodb+srv://userAdmin:thi020206@cluster0.s3hds.mongodb.net/test')
+}
+
+module.exports = conexao
+
